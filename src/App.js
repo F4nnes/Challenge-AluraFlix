38c0b14643a05527c33e86f43788a8549b4e39c5
F4nnes <tonatiuh_06flores@live.com>
2024-07-05 20:34:20 -0600

    "Agregado de banner"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Componentes/Header/Header';
+import Banner from './Componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Banner/>
       
     </div>
   );
---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+
+import "./Banner.css"
+
+
+function Banner() {
+    return <section className="banner-container">
+
+        <img className="banner_fondo"  src="./Imagenes/Banner.png" alt="Banner" />
+
+
+
+    </section>
+}
+
+export default Banner
