9d85ad615af97ba15b01545bf33f12a73b493b84
F4nnes <tonatiuh_06flores@live.com>
2024-07-14 20:13:46 -0600

    "estilizacion de Banner"

--- src/Componentes/Card Banner/CardBanner.js
+++ src/Componentes/Card Banner/CardBanner.js
@@ -1,23 +1,32 @@
 
 
 import FrontTitulo from "../Titulos/Front"
+import VideoBanner from "./caratulaBanner"
+
 import "./CardBanner.css"
 
 
-const CardBanner =()=>{
-    return <div className="card_banner">
+const CardBanner = () => {
+    return <section className="card_banner">
 
         <div className="titulo">
-            <FrontTitulo/>
+            <FrontTitulo />
         </div>
 
-        <h1>Challenge React</h1>
-        <p>Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte
-             en la resolución de un problema para poder aplicar todos los conocimientos adquiridos 
-             en la formación React.</p>
+        <div className="info_banner">
+            <h1>Challenge React</h1>
+            <p>Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte
+                en la resolución de un problema para poder aplicar todos los conocimientos adquiridos
+                en la formación React.
+            </p>
+        </div>
 
+        <div className="vid_sec">
+            <VideoBanner />
 
-    </div>
+        </div>
+
+    </section>
 }
 
 export default CardBanner
---
+++ src/Componentes/Card Banner/caratulaBanner/index.js
@@ -0,0 +1,13 @@
+
+import "./videoBanner.css"
+
+function VideoBanner() {
+
+    return <a href="https://youtu.be/ov7vA5HFe6w?si=rehFqGc0Ux0Z89fj" target="blank">
+        <img className="video_banner" src="./Imagenes/PensarProgramador.png" alt="Pensar como..." />
+    </a>
+
+
+}
+
+export default VideoBanner
