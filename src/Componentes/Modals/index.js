fb77ccf5f300a3674bf7fb0f28f3626dc14ef8e0
F4nnes <tonatiuh_06flores@live.com>
2024-07-14 19:33:38 -0600

    "agregado de Cross para cerrar Modals"

--- src/Componentes/Modals/index.js
+++ src/Componentes/Modals/index.js
@@ -6,18 +6,21 @@ import ListaOpciones from "../ListaOpciones/ListaOpciones";
 import "./editarFormulario.css"
 
 function EditarFormulario() {
-    return <main className="editar_formato">
+    return <dialog className="editar_formato">
 
         <section>
+            <div className="cross">
+                <img className="cerrar" src="./Imagenes/cerrar.png" alt="cerrar" />
+            </div>
             <h1 className="titulo_editar">Editar Card</h1>
 
             <form>
                 <div>
-                    <Campo titulo="Título" placeholder="Título nuevo"/>
+                    <Campo titulo="Título" placeholder="Título nuevo" />
                     <ListaOpciones />
-                    <Campo titulo="Imágen" placeholder="URL de imágen"/>
-                    <Campo titulo="Video" placeholder=" URL de video"/>
-                    <AreaTexto titulo="Descripción" placeholder="Agregar nueva descripción"/>
+                    <Campo titulo="Imágen" placeholder="URL de imágen" />
+                    <Campo titulo="Video" placeholder=" URL de video" />
+                    <AreaTexto titulo="Descripción" placeholder="Agregar nueva descripción" />
                 </div>
 
                 <div className="botons">
@@ -32,7 +35,7 @@ function EditarFormulario() {
 
 
 
-    </main>
+    </dialog>
 }
 
 export default EditarFormulario
